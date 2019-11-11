c56019a18bf5387eaaf4ce09d77691e854227fa4
ZLJXXX <1158301557@qq.com>
2019-11-11 16:17:40 +0800

    //给定一个数，实现任意进制转换

---
+++ 给定一个数  实现任意进制转换/给定一个数  实现任意位置转换/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int M, N;
+	cin >> M >> N;
+	bool flag = false;
+	string str, table = "0123456789ABCDEF";
+	if (N <  2 && N > 16)
+	{
+		return false;
+	}
+	if (M<0)
+	{
+		M = 0 - M;
+		flag = true;
+	}
+	while (M > 0)
+	{
+		str += table[M%N];
+		M = M / N;
+	}
+	if (flag)
+	{
+		str += "-";
+		flag = false;
+	}
+	reverse(str.begin(), str.end());
+	cout << str << endl;
+	system("pause");
+	return 0;
+}
