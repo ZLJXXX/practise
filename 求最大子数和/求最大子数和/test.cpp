b758f26b10d2feca46d460d5819a22ed8bd5baef
ZLJXXX <1158301557@qq.com>
2019-11-12 21:57:36 +0800

    //给定一个数组，求其最大子数组的和

---
+++ 求最大子数和/求最大子数和/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include<vector> 
+#include<algorithm>
+using namespace std;
+//数组中找连续最大子数和
+int main() 
+{ 
+	int size; 
+	cin >> size;
+	vector<int> nums(size); 
+	for (size_t i = 0; i < size; ++i) 
+		cin >> nums[i];
+	int result = nums[0]; 
+	int sum1 = 0, sum2 = 0; 
+	for (int i = 0; i < nums.size(); i++) 
+	{ // 计算到num[i]的子数组的最大和 
+		sum2 = sum1 >= 0 ? sum1+nums[i] : nums[i]; 
+		if(sum2 > result) 
+			result = sum2;
+		
+		if (sum2 < 0) 
+			sum2 = 0;
+		sum1 = sum2;
+	}
+	cout << result << endl; 
+	system("pause");
+	return 0;
+}
