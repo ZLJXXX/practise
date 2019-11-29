9ff1388e3e44eee05cf90fb333302dd1c9b44e51
ZLJXXX <1158301557@qq.com>
2019-11-29 19:27:26 +0800

    //打印菱形

---
+++ 打印菱形/打印菱形/test.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int n = 6;
+	int i, a, b;
+	//前4行.上半部分
+	for (i = 1; i <= n; i++)//控制行数
+	{
+		for (a = n - 1; a >= i; a--)//打印空格
+		{
+			printf(" ");
+		}
+		for (b = 1; b <= 2 * i - 1; b++)//打印*
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//后3行,下半部分
+	for (i = n - 1; i >= 1; i--)
+	{
+		for (a = i; a <= n - 1; a++)
+		{
+			printf(" ");
+		}
+		for (b = 1; b <= 2 * i - 1; b++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
