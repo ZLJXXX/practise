89ed0336aaf2132e1baae9b48102171b3cedad8a
ZLJXXX <1158301557@qq.com>
2019-11-29 19:28:46 +0800

    //将三个数从大到小排序

---
+++ 将三个数从大到小排序/将三个数从大到小排序/test.cpp
@@ -0,0 +1,27 @@
+//将三个数从大到小排列 
+#include <stdio.h>
+int main()
+{
+int a, b, c, t;
+scanf("%d %d %d", &a, &b, &c);
+if (a < b)
+{
+	t = a;
+	a = b;
+	b = t;
+}
+if (a < c)
+{
+	t = a;
+	a = c;
+	c = t;
+}
+if (b < c)
+{
+	t = b;
+	b = c;
+	c = t;
+}
+printf("%d %d %d", a, b, c);
+return 0;
+}
