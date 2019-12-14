d3429fb3ebc9ddf9f38254fd79e3a0a45bd57b78
ZLJXXX <1158301557@qq.com>
2019-12-14 22:20:17 +0800

    //Substr函数

--- Substr函数/Substr函数/test.cpp
+++ Substr函数/Substr函数/test.cpp
@@ -1,10 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS
-//2、substr函数
-//原型：string substr(size_t pos = 0, size_t n = npos) const;
-//功能：获得子字符串。
-//参数说明：pos为起始位置（默认为0），n为结束位置（默认为npos）
-//返回值：子字符串
-
 std::vector<std::string> split(std::string str, std::string pattern)
 {
 	std::string::size_type pos;
@@ -23,3 +17,4 @@ std::vector<std::string> split(std::string str, std::string pattern)
 		}
 	}
 	return result;
+}
